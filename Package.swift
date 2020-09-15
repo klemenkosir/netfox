fc5f26bb968537405f28f2169724dcd69d1b3451
Klemen Kosir <klemen.kosir@icloud.com>
2020-09-15 17:00:47 +0900

    Fix typo in target

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-		.testTarget(name: "netfoxiOS",
+		.target(name: "netfoxiOS",
 					dependencies: [],
 					path: "netfox",
 					exclude: ["OSX"])
